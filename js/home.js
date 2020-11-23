fddd520a6bbcaf0e3728fde4b003ac00b8cbf48f
Lucas Fernando <lucas98fernando@gmail.com>
2020-11-23 15:30:37 -0300

    Transparent navbar in progress

---
+++ js/home.js
@@ -0,0 +1,15 @@
+/*
+var nav = document.getElementById('nav');
+
+window.onscroll = function () {
+
+    if (window.pageYOffset > 100) {
+
+        nav.style.background = "#fafafa";
+        nav.style.boxShadow = "0px 1px 1px #999";
+    } else {
+        nav.style.background = "transparent";
+        nav.style.boxShadow = "none";
+    }
+}
+*/
